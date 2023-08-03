1d7202320a9af3da6321000d520e3257f948669f
Anna-Khvatova <khvatova.ann@yandex.ru>
2023-08-03 21:41:56 +0300

    fix

--- source/js/animation/2d-animation/animation-2d.js
+++ source/js/animation/2d-animation/animation-2d.js
@@ -1,110 +1,110 @@
 import easing from '../../helpers/easing';
 
 export default class Animation {
-    constructor(options) {
-        this.options = options;
+  constructor(options) {
+    this.options = options;
 
-        if (!this.options.easing) {
-            this.options.easing = easing.easeLinear;
-        }
-
-        if (!this.options.duration) {
-            this.options.duration = 1000;
-        }
-
-        if (!this.options.delay) {
-            this.options.delay = 0;
-        }
-
-        if (!this.options.fps) {
-            this.options.fps = 60;
-        }
-
-        this.timeoutId = null;
-        this.requestId = null;
+    if (!this.options.easing) {
+      this.options.easing = easing.easeLinear;
     }
 
-    start(options) {
-        this.stop();
+    if (!this.options.duration) {
+      this.options.duration = 1000;
+    }
 
-        this.timeoutId = setTimeout(() => {
-            this.startTime = performance.now();
-            this.interval = 1000 / this.options.fps;
-            this.lastFrameTime = this.startTime;
-            this.isFinished = false;
+    if (!this.options.delay) {
+      this.options.delay = 0;
+    }
 
-            let animateFrame;
+    if (!this.options.fps) {
+      this.options.fps = 60;
+    }
 
-            if (this.options.duration === `infinite`) {
-                animateFrame = (currentTime) => {
-                    this.requestId = requestAnimationFrame(animateFrame);
+    this.timeoutId = null;
+    this.requestId = null;
+  }
 
-                    const delta = currentTime - this.lastFrameTime;
+  start(options) {
+    this.stop();
 
-                    if (delta > this.interval) {
-                        this.options.func(1, {
-                            startTime: this.startTime,
-                            currentTime,
-                            isFinished: false,
-                            options
-                        });
+    this.timeoutId = setTimeout(() => {
+      this.startTime = performance.now();
+      this.interval = 1000 / this.options.fps;
+      this.lastFrameTime = this.startTime;
+      this.isFinished = false;
 
-                        this.lastFrameTime = currentTime - delta % this.interval;
-                    }
-                };
-            } else {
-                animateFrame = (currentTime) => {
-                    this.requestId = requestAnimationFrame(animateFrame);
+      let animateFrame;
 
-                    const delta = currentTime - this.lastFrameTime;
+      if (this.options.duration === `infinite`) {
+        animateFrame = (currentTime) => {
+          this.requestId = requestAnimationFrame(animateFrame);
 
-                    if (delta > this.interval) {
-                        let timeFraction = (currentTime - this.startTime) / this.options.duration;
+          const delta = currentTime - this.lastFrameTime;
 
-                        if (timeFraction > 1) {
-                            timeFraction = 1;
-                            this.isFinished = true;
-                        }
+          if (delta > this.interval) {
+            this.options.func(1, {
+              startTime: this.startTime,
+              currentTime,
+              isFinished: false,
+              options
+            });
 
-                        if (timeFraction <= 1) {
-                            const progress = this.options.easing(timeFraction);
+            this.lastFrameTime = currentTime - delta % this.interval;
+          }
+        };
+      } else {
+        animateFrame = (currentTime) => {
+          this.requestId = requestAnimationFrame(animateFrame);
 
-                            this.options.func(progress, {
-                                startTime: this.startTime,
-                                currentTime,
-                                isFinished: this.isFinished,
-                                options
-                            });
+          const delta = currentTime - this.lastFrameTime;
 
-                            this.lastFrameTime = currentTime - delta % this.interval;
-                        }
+          if (delta > this.interval) {
+            let timeFraction = (currentTime - this.startTime) / this.options.duration;
 
-                        if (this.isFinished) {
-                            this.stop();
-
-                            if (typeof this.options.callback === `function`) {
-                                this.options.callback();
-                            }
-                        }
-                    }
-                };
+            if (timeFraction > 1) {
+              timeFraction = 1;
+              this.isFinished = true;
             }
 
-            this.requestId = requestAnimationFrame(animateFrame);
-        }, this.options.delay);
+            if (timeFraction <= 1) {
+              const progress = this.options.easing(timeFraction);
+
+              this.options.func(progress, {
+                startTime: this.startTime,
+                currentTime,
+                isFinished: this.isFinished,
+                options
+              });
+
+              this.lastFrameTime = currentTime - delta % this.interval;
+            }
+
+            if (this.isFinished) {
+              this.stop();
+
+              if (typeof this.options.callback === `function`) {
+                this.options.callback();
+              }
+            }
+          }
+        };
+      }
+
+          this.requestId = requestAnimationFrame(animateFrame);
+      }, this.options.delay);
+  }
+
+  restart() {
+    this.start();
+  }
+
+  stop() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
 
-    restart() {
-        this.start();
+    if (this.requestId) {
+      cancelAnimationFrame(this.requestId);
     }
-
-    stop() {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-
-        if (this.requestId) {
-            cancelAnimationFrame(this.requestId);
-        }
-    }
+  }
 }
