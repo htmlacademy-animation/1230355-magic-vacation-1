d7dfebfa7e57a9d21b1ca9aab44a6a381d4fcd33
Anna-Khvatova <khvatova.ann@yandex.ru>
2023-08-23 07:58:28 +0300

    Добавление Extrude объектов для сцены «Замочная скважина» и 4 сцен «Истории» по списку SVG

--- source/js/animation/3d-animation/initAnimationScreen.js
+++ source/js/animation/3d-animation/initAnimationScreen.js
@@ -2,6 +2,6 @@ import { Scene3d } from './scene-3d';
 
 export const scene = new Scene3d({
   elementId: `animation-screen-3d`,
-  cameraConfig: { fov: 35, positionZ: 1250, near: 1, far: 5500 },
+  cameraConfig: { fov: 35, positionZ: 1500, near: 1, far: 5500 },
   enableAnimation: true,
 });
