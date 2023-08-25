f739f36df3484df5b1d7b5ea5c6afbb9ba16e8a1
Anna-Khvatova <khvatova.ann@yandex.ru>
2023-08-25 07:38:07 +0300

    добавлен снеговик, пирамида и два сатурна

--- source/js/animation/3d-animation/3d-objects/snowman.js
+++ source/js/animation/3d-animation/3d-objects/snowman.js
@@ -1,53 +1,56 @@
 import * as THREE from 'three';
 
 export class Snowman extends THREE.Group {
-    constructor() {
-        super();
-        this.constructChildren();
-    }
+  constructor(options) {
+    super();
+    this.colorSphere = options.colorSphere;
+    this.metalnessSphere = options.metalnessSphere;
+    this.roughnessSphere = options.roughnessSphere;
+    this.colorCone = options.colorCone;
+    this.metalnessCone = options.metalnessCone;
+    this.roughnessCone = options.roughnessCone;
+    this.constructChildren();
+  }
 
-    constructChildren() {
-        this.addTopSphere();
-        this.addBottomSphere();
-        this.addCone();
-    }
+  constructChildren() {
+    this.addTopSphere();
+    this.addBottomSphere();
+    this.addCone();
+  }
 
-    addTopSphere() {
-        const material = new THREE.MeshStandardMaterial({
-            color: 0xffffff,
-            metalness: 0.05,
-            emissive: 0x243452,
-            roughness: 0.7
-        });
-        const geometry = new THREE.SphereGeometry(44, 30, 30);
-        const mesh = new THREE.Mesh(geometry, material);
-        this.add(mesh);
-    }
+  addTopSphere() {
+    const material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(this.colorSphere),
+      metalness: this.metalnessSphere,
+      roughness: this.roughnessSphere
+    });
+    const geometry = new THREE.SphereGeometry(44, 30, 30);
+    const mesh = new THREE.Mesh(geometry, material);
+    this.add(mesh);
+  }
 
-    addBottomSphere() {
-        const material = new THREE.MeshStandardMaterial({
-            color: 0xffffff,
-            metalness: 0.05,
-            emissive: 0x243452,
-            roughness: 0.7
-        });
-        const geometry = new THREE.SphereGeometry(78, 30, 30);
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(0, -108, 0);
-        this.add(mesh);
-    }
+  addBottomSphere() {
+    const material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(this.colorSphere),
+      metalness: this.metalnessSphere,
+      roughness: this.roughnessSphere
+    });
+    const geometry = new THREE.SphereGeometry(78, 30, 30);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, -108, 0);
+    this.add(mesh);
+  }
 
-    addCone() {
-        const material = new THREE.MeshStandardMaterial({
-            color: 0xfe4601,
-            metalness: 0.05,
-            emissive: 0x000000,
-            roughness: 0.7
-        });
-        const geometry = new THREE.ConeGeometry(18, 75, 30);
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.rotation.copy(new THREE.Euler(0, 0, THREE.MathUtils.degToRad(-90.0), `XYZ`));
-        mesh.position.set(45, 0, 0);
-        this.add(mesh);
-    }
+  addCone() {
+    const material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(this.colorCone),
+      metalness: this.metalnessCone,
+      roughness: this.roughnessCone
+    });
+    const geometry = new THREE.ConeGeometry(18, 75, 30);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.copy(new THREE.Euler(0, 0, THREE.MathUtils.degToRad(-90.0), `XYZ`));
+    mesh.position.set(45, 0, 0);
+    this.add(mesh);
+  }
 }
--- source/js/animation/3d-animation/sceneController.js
+++ source/js/animation/3d-animation/sceneController.js
@@ -1,79 +1,123 @@
-import { scene } from './initAnimationScreen';
-import { SvgLoader } from './svg-objects/svg-insert';
-import { EXTRUDE_SETTINGS, SVG_FORMS } from '../../helpers/constants';
-import { ExtrudeSvgObjects } from './svg-objects/extrude-svg';
-import { LatheGeometryCreator } from './3d-objects/LatheGeometryCreator';
-import { Saturn } from './3d-objects/saturn';
+import {scene} from './initAnimationScreen';
+import {SvgLoader} from './svg-objects/svg-insert';
+import {EXTRUDE_SETTINGS, SVG_FORMS} from '../../helpers/constants';
+import {ExtrudeSvgObjects} from './svg-objects/extrude-svg';
+// import { LatheGeometryCreator } from './3d-objects/LatheGeometryCreator';
+import {Saturn} from './3d-objects/saturn';
+import {Pyramid} from './3d-objects/pyramid';
+import {Snowman} from './3d-objects/snowman';
+import {reflection3D} from '../../helpers/3d-data';
+import {color3D} from '../../helpers/3d-data';
 
 export const sceneController = {
-    clearScene() {
-        scene.clearScene();
-    },
+  clearScene() {
+    scene.clearScene();
+  },
 
-    async addSvgImages() {
-        const svgShapeLoader = new SvgLoader(SVG_FORMS);
-        const extrudeSvgObjects = new ExtrudeSvgObjects(
-            svgShapeLoader,
-            EXTRUDE_SETTINGS
-        );
+  async addSvgImages() {
+    const svgShapeLoader = new SvgLoader(SVG_FORMS);
+    const extrudeSvgObjects = new ExtrudeSvgObjects(
+        svgShapeLoader,
+        EXTRUDE_SETTINGS
+    );
 
-        const flamingoMesh = await extrudeSvgObjects.createAndAddToScene(
-            SVG_FORMS.flamingo,
-            { depth: 8, bevelThickness: 2, bevelSize: 2 }
-        );
-        const snowflakeMesh = await extrudeSvgObjects.createAndAddToScene(
-            SVG_FORMS.snowflake,
-            { depth: 8, bevelThickness: 2, bevelSize: 2 }
-        );
-        const questionMesh = await extrudeSvgObjects.createAndAddToScene(
-            SVG_FORMS.question,
-            { depth: 8, bevelThickness: 2, bevelSize: 2 }
-        );
-        const leafMesh = await extrudeSvgObjects.createAndAddToScene(
-            SVG_FORMS.leaf,
-            { depth: 8, bevelThickness: 2, bevelSize: 2 }
-        );
+    const flamingoMesh = await extrudeSvgObjects.createAndAddToScene(
+        SVG_FORMS.flamingo,
+        {depth: 8, bevelThickness: 2, bevelSize: 2}
+    );
+    const snowflakeMesh = await extrudeSvgObjects.createAndAddToScene(
+        SVG_FORMS.snowflake,
+        {depth: 8, bevelThickness: 2, bevelSize: 2}
+    );
+    const questionMesh = await extrudeSvgObjects.createAndAddToScene(
+        SVG_FORMS.question,
+        {depth: 8, bevelThickness: 2, bevelSize: 2}
+    );
+    const leafMesh = await extrudeSvgObjects.createAndAddToScene(
+        SVG_FORMS.leaf,
+        {depth: 8, bevelThickness: 2, bevelSize: 2}
+    );
 
-        const flowerMesh = await extrudeSvgObjects.createAndAddToScene(
-            SVG_FORMS.flower,
-            { depth: 4, bevelThickness: 2, bevelSize: 2 }
-        );
+    const flowerMesh = await extrudeSvgObjects.createAndAddToScene(
+        SVG_FORMS.flower,
+        {depth: 4, bevelThickness: 2, bevelSize: 2}
+    );
 
-        const keyholeMesh = await extrudeSvgObjects.createAndAddToScene(
-            SVG_FORMS.keyhole,
-            { depth: 4, bevelThickness: 2, bevelSize: 2 }
-        );
+    const keyholeMesh = await extrudeSvgObjects.createAndAddToScene(
+        SVG_FORMS.keyhole,
+        {depth: 4, bevelThickness: 2, bevelSize: 2}
+    );
 
-        flamingoMesh.position.set(-100, 62, 0);
-        flamingoMesh.rotateX(Math.PI);
+    flamingoMesh.position.set(-100, 62, 0);
+    flamingoMesh.rotateX(Math.PI);
 
-        questionMesh.position.set(0, 200, 0);
-        questionMesh.rotateZ(Math.PI);
-        questionMesh.rotateY(Math.PI);
+    questionMesh.position.set(0, 200, 0);
+    questionMesh.rotateZ(Math.PI);
+    questionMesh.rotateY(Math.PI);
 
-        leafMesh.position.set(120, 0, 0);
+    leafMesh.position.set(120, 0, 0);
 
-        flowerMesh.rotateZ(Math.PI);
-        flowerMesh.position.set(200, -50, 0);
+    flowerMesh.rotateZ(Math.PI);
+    flowerMesh.position.set(200, -50, 0);
 
-        keyholeMesh.position.set(-1000, -1000, -200);
+    keyholeMesh.position.set(-1000, -1000, -200);
 
-        scene.addSceneObject(flamingoMesh);
-        scene.addSceneObject(snowflakeMesh);
-        scene.addSceneObject(questionMesh);
-        scene.addSceneObject(leafMesh);
-        scene.addSceneObject(flowerMesh);
-        scene.addSceneObject(keyholeMesh);
-    },
+    scene.addSceneObject(flamingoMesh);
+    scene.addSceneObject(snowflakeMesh);
+    scene.addSceneObject(questionMesh);
+    scene.addSceneObject(leafMesh);
+    scene.addSceneObject(flowerMesh);
+    scene.addSceneObject(keyholeMesh);
+  },
 
-    async addScreenMesh() {
-      const saturn = new Saturn();
-      saturn.position.set(0, 200, 0);
-      scene.addSceneObject(saturn);
+  addSaturn() {
+    const saturn = new Saturn({
+      colorSaturn: color3D.DominantRed,
+      colorRing: color3D.BrightPurple,
+      colorRope: color3D.MetalGrey,
+      metalness: reflection3D.soft.metalness,
+      roughness: reflection3D.soft.roughness
+    });
+    saturn.position.set(0, 200, 0);
+    scene.addSceneObject(saturn);
+  },
+  addDarkSaturn() {
+    const saturn = new Saturn({
+      colorSaturn: color3D.ShadowedDominantRed,
+      colorRing: color3D.ShadowedBrightPurple,
+      colorRope: color3D.MetalGrey,
+      metalness: reflection3D.soft.metalness,
+      roughness: reflection3D.soft.roughness
+    });
+    saturn.position.set(200, 200, 200);
+    scene.addSceneObject(saturn);
+  },
+  addPyramid() {
+    const pyramid = new Pyramid({
+      pyramidColor: color3D.Blue,
+      metalness: reflection3D.soft.metalness,
+      roughness: reflection3D.soft.roughness
+    });
+    pyramid.position.set(-8, -60, 25);
+    scene.addSceneObject(pyramid);
+  },
+  addSnowman() {
+    const snowman = new Snowman({
+      colorSphere: color3D.SnowColor,
+      metalnessSphere: reflection3D.strong.metalness,
+      roughnessSphere: reflection3D.strong.roughness,
+      colorCone: color3D.Orange,
+      metalnessCone: reflection3D.soft.metalness,
+      roughnessCone: reflection3D.soft.roughness
+    });
+    snowman.position.set(-130, -12, 0);
+    scene.addSceneObject(snowman);
+  },
 
-      const road = new LatheGeometryCreator().createRoad();
-      road.position.set(0, 100, 0);
-      scene.addSceneObject(road);
-      scene.addSceneObject(new LatheGeometryCreator().createCarpet());
-    },
+  async addScreenMesh() {
+    this.addSaturn();
+    this.addDarkSaturn();
+    this.addPyramid();
+    this.addSnowman();
+  },
 };
